1ccf2a3e6b60c75d50d295173c640d4b98b15fa5
Oz4 <aowss18@gmail.com>
2023-01-28 15:40:47 +0300

    add: image

--- src/Stepper.tsx
+++ src/Stepper.tsx
@@ -151,7 +151,7 @@ const Stepper = () => {
                             onClick={() => setStep(prev => prev + 1)}
                             type="button"
                             className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                            Connect Wallet
+                            Next Step
                         </button>
                 }
             </div>
